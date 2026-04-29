13cb2dc36f9cf4140e15043d78cee9934c9da59f
Benethert <marialopezmuelas@gmail.com>
2026-04-29 21:01:10 +0200

    Claves live y test Stripe

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  stripePublicKey: 'CLAVE_STRIPE_QUE_NO_TENGO_AUN',
+  stripePublicKey: 'pk_live_CLAVE_STRIPE',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api',
-  stripePublicKey: 'pk_test_REEMPLAZA_CON_TU_CLAVE_STRIPE',
+  stripePublicKey: 'pk_test_CLAVE_STRIPE',
 };
