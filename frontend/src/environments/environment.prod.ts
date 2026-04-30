24bf7b82b5161b1c23c554b6d064a155b3ab8c3a
Benethert <marialopezmuelas@gmail.com>
2026-04-30 13:50:48 +0200

    Keys nuevas para Stripe y borrado de datos de producción dupicados

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  stripePublicKey: 'pk_live_CLAVE_STRIPE',
+  stripePublicKey: 'pk_live_CLAVE_STRIPE', //No hay porque no se puede poner a producción sin los datos de la empresa
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api',
-  stripePublicKey: 'pk_test_CLAVE_STRIPE',
+  stripePublicKey: 'sk_test_51TRo3NQ2OMWoDUoRYDGE0WEfcajhCWRo8sO5b2d63GutLTgyNeHE53isz76hak22KvGMaWymnIsKFIEmEPDFTZ7600rZRGrgfn',
 };
